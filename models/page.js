eef483ba1317d0f670774e4811803f2819cc40aa
UMANG <umangmalhotra1995@gmail.com>
2018-11-20 22:09:51 +0530

    Added Page Schema

---
+++ models/page.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+// Page Schema
+
+const PageSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    slug: {
+        type: String
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    sorting: {
+        type: Number
+    }
+});
+
+const Page = module.exports = mongoose.model('Page',PageSchema);
